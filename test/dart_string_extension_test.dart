fa237080e627094796c66abddf6133f11276b520
yusuf umar <yusuf.umar@academicianhelp.com>
2022-11-24 08:11:14 +0100

    fest: write test for toAllCapital, toSentenceCase, toTitleCase

--- test/dart_string_extension_test.dart
+++ test/dart_string_extension_test.dart
@@ -1,8 +1,15 @@
-// import 'package:dart_string_extension/dart_string_extension.dart';
-// import 'package:test/test.dart';
+import 'package:dart_string_extension/dart_string_extension.dart';
+import 'package:test/test.dart';
 
-// void main() {
-//   test('calculate', () {
-//     expect(calculate(), 42);
-//   });
-// }
+void main() {
+
+  test("testing toAllCapital case", () {
+    expect('Hello'.allToCapital, "HELLO");
+  });
+  test("testing toSentense case", () {
+    expect('hello MR MAN'.toSentenceCase, "Hello mr man");
+  });
+  test("testing toTitleCase case", () {
+    expect('hello this is a test'.toTitleCase, "Hello This Is A Test");
+  });
+}
